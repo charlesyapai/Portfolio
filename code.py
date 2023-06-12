30aac1ce3c65aea80f81e687d9d6fc6cc2cf0b7c
GryphonHolder <chaaarlieyap@gmail.com>
2023-06-12 11:52:36 +0800

    new

--- code.py
+++ code.py
@@ -1,14 +1,41 @@
-import os
 import streamlit as st
+import streamlit.components.v1 as components
 
-# Get the absolute path to the CSS file
-css_path = os.path.join(os.path.dirname(__file__), 'styles.css')
+# Specify html code below
+components.html(
+    """
+    <style>
+        .multicolor {
+            font-family: Arial, sans-serif;
+            background-image: linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet);
+            background-clip: text;
+            -webkit-background-clip: text;
+            color: transparent;
+        }
 
-# Refer to the CSS file using st.markdown
-st.markdown(f'<link href="{css_path}" rel="stylesheet">', unsafe_allow_html=True)
+        .big-blue {
+            font-family: Arial, sans-serif;
+            font-size: 24px;
+            color: blue;
+        }
 
-# Your remaining Streamlit code
-st.title("My Portfolio")
-st.write("I am a junior data scientist specializing in Python and Machine Learning.")
-st.write("Project 1 - [GitHub Link](https://github.com/GryphonHolder/Project_1)")
-st.write("Project 2 - [GitHub Link](https://github.com/GryphonHolder/GAProj2)")
+        .cursive-red {
+            font-family: cursive;
+            color: red;
+        }
+    </style>
+    <h1> My Portfolio </h1>
+    <div> I am a junior data scientist specializing in Python and Machine Learning.</div>
+    <hr />
+    <div> Project 1 - <a href="https://github.com/GryphonHolder/Project_1" target="_blank">GitHub Link</a> </div>
+    <div> Project 2 - <a href="https://github.com/GryphonHolder/GAProj2" target="_blank">GitHub Link</a> </div>
+    <div class="multicolor">Multicolor Text</div>
+    <div class="big-blue">Big and Blue Text</div>
+    <div class="cursive-red">Cursive and Red Text</div>
+    """,
+    height=600,
+)
+
+
+
+#streamlit run "/Users/charles/Desktop/GitHub/Portfolio/code.py"
