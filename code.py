d90d9f707707951bec79d5090f25e96ed41db00c
GryphonHolder <35774613+GryphonHolder@users.noreply.github.com>
2023-06-12 11:47:56 +0800

    Update code.py

--- code.py
+++ code.py
@@ -1,22 +1,14 @@
+import os
 import streamlit as st
-import streamlit.components.v1 as components
 
-# Specify html code below
-components.html(
-    """
-    <style>
-        body {
-            background-image: url('https://graduate.northeastern.edu/resources/wp-content/uploads/sites/4/2020/06/iStock-1221293664-1.jpg');
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
-        }
-    </style>
-    <h1> My Portfolio </h1>
-    <div> I am a junior data scientist specializing in Python and Machine Learning.</div>
-    <hr />
-    <div> Project 1 - <a href="https://github.com/GryphonHolder/Project_1" target="_blank">GitHub Link</a> </div>
-    <div> Project 2 - <a href="https://github.com/GryphonHolder/GAProj2" target="_blank">GitHub Link</a> </div>
-    """,
-    height=600,
-)
+# Get the absolute path to the CSS file
+css_path = os.path.join(os.path.dirname(__file__), 'styles.css')
+
+# Refer to the CSS file using st.markdown
+st.markdown(f'<link href="{css_path}" rel="stylesheet">', unsafe_allow_html=True)
+
+# Your remaining Streamlit code
+st.title("My Portfolio")
+st.write("I am a junior data scientist specializing in Python and Machine Learning.")
+st.write("Project 1 - [GitHub Link](https://github.com/GryphonHolder/Project_1)")
+st.write("Project 2 - [GitHub Link](https://github.com/GryphonHolder/GAProj2)")
